56ece1806404935ad7fdee5c107f269e36b713aa
Okinea Dev <hi@okinea.dev>
2025-11-26 08:18:37 +0100

    chore: move readme to `packages/toon/README.md` and symlink in the pr… (#222)
    * chore: move readme to `packages/toon/README.md` and symlink in the project root
    
    * fix paths and exclude `packages/toon/README.md` from `eslint` to avoid double linting

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,6 +5,9 @@ export default antfu({
   rules: {
     'no-cond-assign': 'off',
   },
+  // `README.md` is symlinked to this file so we
+  // exclude it to avoid linting the same file twice.
+  ignores: ['packages/toon/README.md'],
 }).append({
   files: ['README.md', 'SPEC.md', '**/docs/**/*'],
   rules: {
